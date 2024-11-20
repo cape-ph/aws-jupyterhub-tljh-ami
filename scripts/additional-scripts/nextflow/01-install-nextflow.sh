e10aa5cab6c2a0a574e499bf445be17282c0e583
Drew Pihera <dpihera@gmail.com>
2024-11-20 13:01:48 -0500

    removed OBE script

--- scripts/additional-scripts/nextflow/01-install-nextflow.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# download nextflow and build it
-curl -s https://get.nextflow.io | bash
-# make the created binary executable
-chmod +x nextflow
-# move it into place
-sudo mv nextflow /usr/local/bin
